f19531723ff733e0647dc1d29560f9dcef45dd63
sigmanificient <edhyjox@gmail.com>
2022-01-08 17:12:32 +0100

    :lipstick: Bas css

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,38 @@
 <template>
-    <h1>Hello, World!</h1>
+	<p>Hello World</p>
 </template>
 <script>
 export default {
-    name: 'App'
+	name: 'App'
 }
 </script>
+<style lang="scss">
+:root {
+	scroll-behavior: smooth;
+
+	--color-bg: #fff;
+
+	--color-text: #000;
+	--color-text-light: #838383;
+
+	--color-primary: #fE7624;
+	--color-secondary: #205eff;
+}
+
+body {
+	margin: 0;
+
+	font-size: clamp(10px, 1.5vw, 29px);
+	font-family: 'Open Sans', sans-serif;
+
+	ul {
+		list-style: none;
+	}
+
+	a {
+		color: inherit;
+		text-decoration: none;
+	}
+}
+
+</style>
