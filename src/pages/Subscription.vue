daa43fbebcfe3ed4b1b51ff1687d6b748e4420b0
sigmanificient <edhyjox@gmail.com>
2022-01-09 02:08:15 +0100

    :sparkles: Adding subscription base html

---
+++ src/pages/Subscription.vue
@@ -0,0 +1,95 @@
+<template>
+	<p class="ultra-big">
+		Our <b>plans</b> for your <b>strategies.</b>
+	</p>
+	<div class="plans">
+		<div id="monthly" class="plan-card">
+			<p class="price">
+				<b>1499</b>
+				<sub>/month</sub>
+			</p>
+			<p class="fee-type">
+				Monthly
+			</p>
+
+			<div class="benefit">
+				<p class="benefit-item">
+					<img src="../assets/icons/checks/blue.svg" alt="+"/>
+					1 Owner account
+				</p>
+				<p class="benefit-item">
+					<img src="../assets/icons/checks/blue.svg" alt="+"/>
+					Up to 5 worker accounts.
+				</p>
+				<p class="benefit-item">
+					<img src="../assets/icons/checks/blue.svg" alt="+"/>
+					24 X 7 support for 30 days.
+				</p>
+				<a class="btn btn__blue" href="/">Select</a>
+			</div>
+		</div>
+		<div id="annually" class="plan-card best">
+			<p class="price">
+				<b>14999</b>
+				<sub>/year</sub>
+			</p>
+			<p class="fee-type">
+				Yearly
+			</p>
+
+			<div class="benefit">
+				<p class="benefit-item">
+					<img src="../assets/icons/checks/blue.svg" alt="+"/>
+					1 Owner account
+				</p>
+				<p class="benefit-item">
+					<img src="../assets/icons/checks/blue.svg" alt="+"/>
+					Up to 5 worker accounts.
+				</p>
+				<p class="benefit-item">
+					<img src="../assets/icons/checks/blue.svg" alt="+"/>
+					24 X 7 support for 30 days.
+				</p>
+				<p class="benefit-item">
+					<img src="../assets/icons/checks/blue.svg" alt="+"/>
+					Save 3000 INR.
+				</p>
+				<a class="btn btn__orange" href="/">Select</a>
+			</div>
+
+		</div>
+		<div id="entreprise" class="plan-card">
+			<p class="price">
+				<b>Custom</b>
+				<sub>Enterprise</sub>
+			</p>
+			<p class="fee-type">
+				Yearly
+			</p>
+
+			<div class="description">
+				<div class="description-reason">
+					Need more than 5 worker accounts?
+				</div>
+				<p>
+					<b>or</b>
+				</p>
+				<div class="description-reason">
+					Want a dedicated support & training team?
+				</div>
+			</div>
+
+			<a class="btn btn__black" href="/">Contact</a>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Subscription"
+}
+</script>
+
+<style scoped>
+
+</style>
