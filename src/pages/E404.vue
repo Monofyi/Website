1e6d0e8d4caad1a32bc2f33b949d68aa139b997a
sigmanificient <edhyjox@gmail.com>
2022-01-09 02:47:52 +0100

    :sparkles: 404 page

---
+++ src/pages/E404.vue
@@ -0,0 +1,17 @@
+<template>
+	<h1>Page not Found</h1>
+	<p>
+		Sorry but the page you are looking for does not exist.
+		<router-link to="/">Go to Home</router-link>
+	</p>
+</template>
+
+<script>
+export default {
+	name: "404"
+}
+</script>
+
+<style scoped>
+
+</style>
