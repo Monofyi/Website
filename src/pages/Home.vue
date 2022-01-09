8522e07fe0c74f7445a632d769269667836c5c3b
sigmanificient <edhyjox@gmail.com>
2022-01-09 01:11:10 +0100

    :sparkles: Adding home html structure

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,189 @@
 <template>
-	<h1>Home!</h1>
+	<div class="landing">
+		<div class="left">
+			<h1>Welcome to Bitecope!</h1>
+			<p>
+				Manage purchase, inventory-stock, production, shipment effectively |
+				Realtime Updates | Scalable | Sustainable | Secure
+			</p>
+		</div>
+		<div class="right">
+			<img
+					src="../assets/illustrations/people/reading.svg"
+					alt="Manage your business in realtime, with scalable features."
+			/>
+		</div>
+	</div>
+	<div id="about_us" class="about_us">
+		<h2>About Us!</h2>
+
+		<div class="illustrations">
+			<img
+					src="../assets/illustrations/people/balancing.svg"
+					alt="Bitecope allows for efficient supply chain management"
+			/>
+			<img
+					src="../assets/illustrations/people/running.svg"
+					alt="Bitecope is the future of supply chain management in india"
+			/>
+			<img
+					src="../assets/illustrations/people/working.svg"
+					alt="Bitecope company is aiming to reduce waste in production"
+			/>
+		</div>
+
+		<p>
+			Are you having a hard time coordinating with multiple departments in your
+			business? Tired of dealing with existing issues arising due to
+			miscommunication across departments? With Bitecope, say no more!
+		</p>
+		<p>
+			Bitecope is one of a kind company which designs tools to be used by the
+			owner and employees of businesses, making the whole process hassle-free,
+			coordinated and online. We provide multi-channel accessibility through our
+			mobile app and website.
+		</p>
+	</div>
+	<div class="perks">
+		<h2>Perks Of Using Bitecope</h2>
+		<div class="perks--wrapper">
+			<div class="perks-item">
+				<h3 class="number">1</h3>
+				<p>
+					<b>
+						Access to fully co-ordinated inventory management website containing
+						real-time data.
+					</b>
+				</p>
+			</div>
+			<div class="perks-item">
+				<h3 class="number">2</h3>
+				<p>
+					<b>
+						Access to mobile application for ordering the products in real-time.
+					</b>
+				</p>
+			</div>
+			<div class="perks-item">
+				<h3 class="number">3</h3>
+				<p>
+					<b>
+						Quicker and accurate decision-making - from buying, to production
+						and consumption.
+					</b>
+				</p>
+			</div>
+			<div class="perks-item">
+				<h3 class="number">4</h3>
+				<p>
+					<b>
+						Less Wastage of products.
+					</b>
+				</p>
+			</div>
+			<div class="perks-item">
+				<h3 class="number">5</h3>
+				<p>
+					<b>
+						Effective tracking of all products - expired, excess, shortage, sold
+						etc.
+					</b>
+				</p>
+			</div>
+			<div class="perks-item">
+				<h3 class="number">6</h3>
+				<p>
+					<b>
+						Effective cost-cutting and hence more profits.
+					</b>
+				</p>
+			</div>
+			<div class="perks-item">
+				<h3 class="number">7</h3>
+				<p>
+					<b>
+						No delay in the whole process whatsoever.
+					</b>
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="supply_chain">
+		<h2>Manufacturing the Bitecope Way!</h2>
+
+		<div class="supply_chain--container">
+			<div class="step">
+				<img
+						src="../assets/illustrations/steps/1.svg"
+						alt="Step 1"
+				/>
+				<div class="description">
+					<h3>the plan or strategy</h3>
+					<p>
+						The supply chain manager tries to minimize shortages and keep costs
+						down. The job is not only about logistics and purchasing inventory
+						but also to improve productivity, quality, and efficiency of
+						operations.
+					</p>
+				</div>
+			</div>
+			<div class="step">
+				<img
+						src="../assets/illustrations/steps/2.svg"
+						alt="Step 2"
+				/>
+				<div class="description">
+					<h3>The source of raw materials or service</h3>
+					<p>
+						The owner can coordinate all the operations across departments and
+						have an accurate idea of resources required, surplus, etc. based on
+						the real-time demand.
+					</p>
+				</div>
+			</div>
+			<div class="step">
+				<img
+						src="../assets/illustrations/steps/3.svg"
+						alt="Step 3"
+				/>
+				<div class="description">
+					<h3>Distribution</h3>
+					<p>
+						The distributor can make early decision about the stocks based on
+						the re-order level if he gets to see real-time data updates about
+						products.
+					</p>
+				</div>
+			</div>
+			<div class="step">
+				<img
+						src="../assets/illustrations/steps/4.svg"
+						alt="Step 4"
+				/>
+				<div class="description">
+					<h3>Delivery and logistics</h3>
+					<p>
+						The retailer can keep track of goods delivered, not delivered, excess
+						as well as expired goods making warehouse management easier!
+					</p>
+				</div>
+			</div>
+			<div class="step">
+				<img
+						src="../assets/illustrations/steps/4.svg"
+						alt="Step 5"
+				/>
+				<div class="description">
+					<h3>The return System</h3>
+					<p>
+						Retailer can return excess goods or goods that are not sold to the
+						distributor. Those good can be redirected to the required warehouse
+						location.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
