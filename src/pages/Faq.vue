c9da1972103d7408330690c40a09dbfb7e0df7b0
sigmanificient <edhyjox@gmail.com>
2022-01-09 02:35:45 +0100

    :sparkles: Adding FAQ base html

---
+++ src/pages/Faq.vue
@@ -0,0 +1,47 @@
+<template>
+	<h1>FAQ</h1>
+	<h2>PRICING</h2>
+	<h3>What are the charges for Bitecope app?</h3>
+	<p>
+		Bitecope is based on monthly subscription model (for more details contact
+		support@bitecope.com), however user are provided with 30 day free trial for
+		the first instance when they Sign up. So that, user can try & test the app
+		features and assess its use-case before buying it.
+	</p>
+
+	<h2>PRODUCT RELATED</h2>
+
+	<h3>What kind of businesses can use this app?</h3>
+	<p>
+		Manufacturing companies belonging to any sectors can use this app. Size of
+		the company also does not	matter.
+	</p>
+
+	<h3>Where can we download Bitecope Desktop app?</h3>
+	<p>
+		Desktop Version is still under development phase. Once it's launched we will
+		notify all our users.	Your patience is highly appreciated.
+	</p>
+
+	<h3>How can I manage worker permission?</h3>
+	<p>Not yet! This feature will be implemented soon.</p>
+
+	<h3>Can I access the same Bitecope account from two mobile devices?</h3>
+	<p>Yes, you can do it.</p>
+
+	<h3>Is Bitecope Cloud/Web based?</h3>
+	<p>
+		Bitecope is still in process of launching this services, Once it's launched
+		we will notify all our users. Your patience is highly appreciated.
+	</p>
+</template>
+
+<script>
+export default {
+	name: "Faq"
+}
+</script>
+
+<style scoped>
+
+</style>
